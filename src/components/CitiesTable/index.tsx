08e98376f130959c93cb08992fb80f35aa79ef31
_rdx98 <elder_louzada@outlook.com>
2020-02-07 02:33:16 -0300

    implements CitiesTable component

---
+++ src/components/CitiesTable/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import cities from '../../json/city.list.json'
+import City from '../../json/city.interface'
+import geodist from 'geodist'
+import './style.css'
+const brazilianCities = (cities as City[]).filter(city => city.country === 'BR')
+
+interface CitiesTableProps {
+    coord: {
+        lat: number,
+        lon: number
+    },
+    cityName?: string
+}
+
+const CitiesTable: React.FC<CitiesTableProps> = ({cityName, coord} : CitiesTableProps) => {
+
+    const sortCitiesArr = (citiesArr : City[]) => citiesArr.sort((a,b) => geodist(coord, a.coord) - geodist(coord, b.coord))
+
+    const filterCitiesArr = (citiesArr : City[]) => citiesArr.filter(({ name }) => cityName ? name.toLowerCase().startsWith(cityName.toLowerCase()): true)
+
+    const createTableRow = (citiesArr : City[]) => citiesArr.map(({ id, name, coord, country }) =>
+        <tr key={id}>
+            <td>{id}</td>
+            <td>{name}</td>
+            <td>{country}</td>
+            <td>{coord.lat}</td>
+            <td>{coord.lon}</td>
+        </tr>)
+
+    return <table className="cities-table">
+        <thead>
+            <tr>
+                <th>Código</th>
+                <th>Nome</th>
+                <th>País</th>
+                <th>Latitude</th>
+                <th>Longitude</th>
+            </tr>
+        </thead>
+        <tbody>
+            {createTableRow(sortCitiesArr(filterCitiesArr(brazilianCities)))}
+        </tbody>
+    </table>
+}
+
+export default CitiesTable
